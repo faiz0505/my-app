7409f73044f1e743977fbb47db84c5e566a63e9b
Faiz Al <faizali786313@gmail.com>
2024-03-15 15:12:49 +0530

    re-build to solve auth problem

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { allPosts } from "@/app/actions/post.actions";
+import { auth } from "@clerk/nextjs";
 // import { getFollowes } from "../actions/follow.action";
 const page = async () => {
-  const posts = await allPosts();
+  const { userId } = auth();
+  const posts = await allPosts(userId);
   // const data = await getFollowes();
   return (
     <main className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 px-2 md:px-8 lg:px-20 py-5">
--- app/actions/post.actions.jsx
+++ app/actions/post.actions.jsx
@@ -3,20 +3,17 @@
 import { dbConnection } from "@/lib/db/connection";
 import { postModel } from "@/lib/db/models";
 import PostCard from "@/app/components/PostCard";
-import { auth } from "@clerk/nextjs";
 import { ErrorHandler } from "@/utils/errorHandler";
-const { userId } = auth();
-export const allPosts = async () => {
+export const allPosts = async (userId) => {
   // console.log(userId);
   try {
     await dbConnection();
     const posts = await postModel.find();
     return posts.map((post) => {
-      // console.log(post.user.toString());
       return (
         <PostCard
           key={post._id}
-          isAuthenticated={userId === post.user && true}
+          isAuthenticated={post.user.toString() === userId}
           user={post.user.toString()}
           caption={post.caption}
           imageUrl={post.imageUrl}
@@ -28,11 +25,11 @@ export const allPosts = async () => {
   }
 };
 
-export const userPosts = async () => {
+export const userPosts = async (userId) => {
   try {
     await dbConnection();
     const allPosts = await postModel.find();
-    const posts = allPosts.filter((post) => post.user === userId);
+    const posts = allPosts.filter((post) => post.user.toSting() === userId);
 
     return JSON.stringify(posts);
   } catch (error) {
